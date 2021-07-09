44a4b10c82c826c950dcbaee8a091689c42d44da
Tin Tran <tintran271195@gmail.com>
2021-07-09 16:07:52 +0700

    already to deloy

---
+++ demo1/src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "HOME";
+    }
+}
